058e09f9f3192d805638643f814b0d397807a61d
Jacob Baca <52418765+jacobrobertbaca@users.noreply.github.com>
2020-04-10 15:33:52 -0400

    bumping patch (#28)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-mailchimp',
-      version='1.0.2',
+      version='1.0.3',
       description='Singer.io tap for extracting data from the Mailchimp API',
       author='Stitch',
       url='https://singer.io',
